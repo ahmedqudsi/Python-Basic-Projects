d1a3bcf291bf06bfde220010d855aff06f1d31c0
ahmedqudsi <ahmedqudsi786@gmail.com>
2025-12-13 16:58:43 +0530

    madlib game

---
+++ madlib game.py
@@ -0,0 +1,14 @@
+# creates a story by filling the blanks
+
+ad = input("Adjective for car: ")
+noun = input("Noun describe the car: ")
+verb = input(" use ing verb: ")
+ad2 = input("Adjective for owner: ")
+ad3 = input("Adjective for feeling: ")
+verb2 = input("verb for action: ")
+
+
+print(f"Today I saw a {ad} car.")
+print(f" On a closer look it was {noun} and {verb} it was one of my dreams of childhood.")
+print(f" The owner of the car war a {ad2} man who gave me a chance to drive the car.")
+print(f" I was so {ad3} that I could {verb2} all day long.")
